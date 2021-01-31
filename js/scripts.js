c57a54794e4fe5e8558b800b3b88582c8a75cf1f
Chris Ratterree <ratt18@hotmail.com>
2021-01-31 14:16:56 -0700

    Format

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(function() {
-    $("#refinishModalButton").on("click",function(){
-        $("#refinishModal").modal("show");
-    });
-    $("#restoModalButton").on("click",function(){
-        $("#restoModal").modal("show");
-    });
-    $("#racecarModalButton").on("click",function(){
-        $("#racecarModal").modal("show");
-    });
+$(function () {
+  $("#refinishModalButton").on("click", function () {
+    $("#refinishModal").modal("show");
+  });
+  $("#restoModalButton").on("click", function () {
+    $("#restoModal").modal("show");
+  });
+  $("#racecarModalButton").on("click", function () {
+    $("#racecarModal").modal("show");
+  });
 });
